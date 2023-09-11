45564178b607821c341274c9752df9a4cc848207
Artūrs <akempelis@gmail.com>
2023-09-11 13:56:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
+
+
+xpoints = np.array([0, 7])
+ypoints = np.array([0, 260])
 
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
