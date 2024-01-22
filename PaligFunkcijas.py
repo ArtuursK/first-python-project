93fa9c477742695f383aea08c06f4af30242de6a
IZV\adm_ak <arturs@arturs.com>
2024-01-22 13:48:43 +0200

    Add unit test samples

--- PaligFunkcijas.py
+++ PaligFunkcijas.py
@@ -2,5 +2,10 @@
 # funkcijas argumenti - ieejas
 # funkcijas rezultāts - izeja
 def summa(a, b):
-    return a + b
+    # parbauda vai argumenti ir skaitliskas vertibas:
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a + b
+    else:
+        return "Saskaitāmie argumenti nav skaitliskas vērtības"
+
 
---
+++ tests/TestPaligFunkcijas.py
@@ -0,0 +1,8 @@
+
+import PaligFunkcijas
+
+assert PaligFunkcijas.summa(2, 3) == 5, "Jābūt 5"
+assert PaligFunkcijas.summa('a', 'c') == "Saskaitāmie argumenti nav skaitliskas vērtības"
+assert PaligFunkcijas.summa('a', 2) == "Saskaitāmie argumenti nav skaitliskas vērtības"
+
+
